e30b21e25e9213d8f5648db15e2d404f3df8c1ad
cdrani <charlesdrani@gmail.com>
2024-03-30 15:08:26 -0600

    feat: prevent menubar view closing on focus lost

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -55,15 +55,16 @@ fn main() {
                 _ => {}
             }
         })
-        .on_window_event(|event| match event.event() {
-            tauri::WindowEvent::Focused(is_focused) => {
-                // detect click outside of the focused window and hide the app
-                if !is_focused {
-                    event.window().hide().unwrap();
-                }
-            }
-            _ => {}
-        })
+        // TODO: uncomment prior to release. In this state for better dev flow
+        // .on_window_event(|event| match event.event() {
+        //     tauri::WindowEvent::Focused(is_focused) => {
+        //         // detect click outside of the focused window and hide the app
+        //         if !is_focused {
+        //             event.window().hide().unwrap();
+        //         }
+        //     }
+        //     _ => {}
+        // })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
