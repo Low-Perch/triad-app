8bab1fb016886b2ea267279e2580413d8f95c75a
cdrani <charlesdrani@gmail.com>
2024-03-31 15:02:32 -0600

    feat: create Info view for modal

---
+++ src/lib/views/Info.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-col w-full h-full text-white p-5 justify-center gap-3 items-center">
+    <h2 class="font-semibold text-lg text-center">How to Play</h2>
+
+    <p class="text-md">
+        Given 3 clue fragments, find the word fragment that completes all 3 clues to form full words.
+    </p>
+
+    <div>
+        <p class="font-semibold text-md">Example:</p>
+        <p class="text-md">TUN/GON/IN + DRA = TUNDRA/DRAGON/DRAIN
+    </div>
+
+    <hr class="w-full h-1 text-white" />
+
+    <ul class="list-disc list-inside text-md">
+        <li>Any of the 3 clue fragments may be starting or ending fragments. Their order is random.</li>
+        <li>If you feel stuck, use any of the 3 lifelines available in the top right corner.</li>
+    </ul>
+
+    <div class="flex justify-center h-full items-center">
+        <button class="items-center inline-flex justify-center w-24 h-10 border-2 rounded-3xl border-white">
+            <span class="text-md">Play</span>
+        </button>
+    </div>
+</div>
