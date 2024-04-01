5d4efdececaece0b98becc1be97b9ffbd87fee02
cdrani <charlesdrani@gmail.com>
2024-04-01 17:59:20 -0600

    feat: create Clues component

---
+++ src/lib/components/Clues.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<div class="relative flex-col mt-10 gap-y-24 w-11/12 uppercase text-lg justify-center items-center mx-auto">
+    <div>
+        <p class="text-3xl text-center">ware / reaf / con</p>
+        <p id="clue-position" class="hidden text-3xl text-center tracking-widest">ware / reaf____  / con</p>
+    </div>
+
+    <p id="full-solution" class="hidden text-3xl text-center">firmware / reaffirm / confirm</p>
+</div>
