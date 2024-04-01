73824b845a3a6c76c2a7d99cbc4d27fe93af8834
cdrani <charlesdrani@gmail.com>
2024-04-01 17:59:35 -0600

    feat: create Input component

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+</script>
+
+<div class="flex justify-center items-center my-8">
+    <div class="flex m-4 aspect-square border-white border-2 w-12 justify-center place-items-center">
+        <p class="text-white uppercase text-xl">f</p>
+    </div>
+
+    <div class="flex m-4 aspect-square border-white border-2 w-12 justify-center place-items-center">
+        <p class="text-white uppercase text-xl">i</p>
+    </div>
+
+    <div class="flex m-4 aspect-square border-white border-2 w-12 justify-center place-items-center">
+        <p class="text-white uppercase text-xl">m</p>
+    </div>
+
+    <div class="flex m-4 aspect-square border-white border-2 w-12 justify-center place-items-center">
+        <p class="text-white uppercase text-xl">m</p>
+    </div>
+</div>
