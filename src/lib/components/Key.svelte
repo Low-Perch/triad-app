749b77e3aac0b71a847b711da812e8067755cb53
cdrani <charlesdrani@gmail.com>
2024-04-02 11:29:22 -0600

    feat: add Keys event to update input state

--- src/lib/components/Key.svelte
+++ src/lib/components/Key.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
+    import { addKey, removeKey } from '../stores/input'
+
     export let key: string;
     export let width: number = 8;
+
+    function handleKey(e: Event)  {
+        const button = e.currentTarget as HTMLButtonElement
+        const key = button.name.toUpperCase()
+
+        if (key == "GO") {
+            // TODO
+        } else if (key == "DEL") {
+            removeKey()
+        } else {
+            addKey(key)
+        }
+    }
 </script>
 
-<button name={key} class="h-10 w-{width} rounded-sm bg-gray-700 hover:bg-gray-500">
+<button 
+    name={key}
+    on:click|preventDefault|stopPropagation={handleKey}
+    class="h-10 w-{width} rounded-sm bg-gray-700 hover:bg-gray-500"
+>
     <span class="text-sm font-semibold uppercase">{key}</span>
 </button>
