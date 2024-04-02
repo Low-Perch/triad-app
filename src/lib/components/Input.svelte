80ce6f6aaa752177129000992c912b05e6f859f5
cdrani <charlesdrani@gmail.com>
2024-04-02 11:31:35 -0600

    feat: connect Input ui with store values

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -1,20 +1,13 @@
 <script lang="ts">
+    import { getInput } from "../stores/input";
+
+    const input = getInput()
 </script>
 
-<div class="flex justify-center items-center my-8">
-    <div class="flex m-4 aspect-square border-white border-2 w-12 justify-center place-items-center">
-        <p class="text-white uppercase text-xl">f</p>
-    </div>
-
-    <div class="flex m-4 aspect-square border-white border-2 w-12 justify-center place-items-center">
-        <p class="text-white uppercase text-xl">i</p>
-    </div>
-
-    <div class="flex m-4 aspect-square border-white border-2 w-12 justify-center place-items-center">
-        <p class="text-white uppercase text-xl">m</p>
-    </div>
-
-    <div class="flex m-4 aspect-square border-white border-2 w-12 justify-center place-items-center">
-        <p class="text-white uppercase text-xl">m</p>
-    </div>
+<div class="flex justify-center items-center my-8 h-40">
+    {#each $input.keys as key}
+        <div key={key} class="flex m-4 aspect-square border-white border-2 w-12 h-12 justify-center place-items-center">
+            <p class="text-white uppercase text-xl">{key}</p>
+        </div>
+    {/each}
 </div>
