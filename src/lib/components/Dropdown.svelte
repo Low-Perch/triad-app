59695b30ae4162c4db2d6e5408eb5dd2d5605034
cdrani <charlesdrani@gmail.com>
2024-04-02 14:51:08 -0600

    refactor: extract Dropdown into component

---
+++ src/lib/components/Dropdown.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    import { closeModal } from "../stores/modal"
+    import DropdownItem from "./DropdownItem.svelte";
+
+    // TODO: move into store
+    const clues = [
+        { id: 'position', active: false, note: 'Reveal 1 position' },
+        { id: 'letter', active: false, note: 'Reveal last letter' },
+        { id: '50/50', active: false, note: '50/50' }
+    ]
+
+    function toggleClues() {
+        const dropdown = document.getElementById('dropdown') as HTMLButtonElement
+        if (!dropdown) return
+
+        closeModal()
+
+        const isHidden = dropdown.classList.contains("hidden")
+        isHidden ? dropdown.classList.remove("hidden") : dropdown.classList.add("hidden")
+    }
+
+    function handleClue(e: Event) {
+        const element = e.currentTarget as HTMLLIElement
+        const clueType = element.id
+
+        toggleClues()
+
+        if (clueType == "letter") {
+            // TODO
+        } else if (clueType == "position") {
+            // TODO
+        } else {
+            // TODO
+        }
+    }
+</script>
+
+<div tabIndex="0" class="group relative inline-block">
+    <button class="w-9 h-9 flex items-center place-content-center hover:rounded-full hover:bg-gray-600" id="support" name="support" on:click|preventDefault|stopPropagation={toggleClues}>
+        <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 64 64" width="20">
+            <g fill="#fff">
+                <path d="m32 16c2.961 0 5.699.859 8.078 2.262l11.52-11.52c-5.418-4.211-12.207-6.742-19.598-6.742-7.395 0-14.18 2.531-19.602 6.742l11.52 11.523c2.379-1.406 5.121-2.265 8.082-2.265z"/>
+                <path d="m57.262 12.406-11.524 11.516c1.403 2.379 2.262 5.117 2.262 8.078s-.859 5.703-2.262 8.078l11.52 11.52c4.211-5.418 6.742-12.207 6.742-19.598s-2.531-14.18-6.738-19.594z"/>
+                <path d="m40.078 45.738c-2.379 1.403-5.117 2.262-8.078 2.262s-5.699-.859-8.078-2.262l-11.52 11.52c5.418 4.211 12.207 6.742 19.598 6.742s14.18-2.531 19.598-6.742z"/>
+                <path d="m16 32c0-2.961.859-5.699 2.262-8.078l-11.52-11.52c-4.211 5.418-6.742 12.207-6.742 19.598 0 7.395 2.531 14.18 6.742 19.602l11.52-11.523c-1.403-2.376-2.262-5.118-2.262-8.079z"/>
+            </g>
+        </svg>
+    </button>
+
+    <ul id="dropdown" class="absolute z-20 mt-2 hidden bg-gray-700 border-gray-200 border-2 text-sm -left-6 w-36 text-white list-none">
+        {#each clues as clue (clue.id) }
+            <DropdownItem clue={clue} on:message={handleClue} />
+        {/each}
+    </ul>
+</div>
---
+++ src/lib/components/DropdownItem.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte'
+    export let clue: { id: string, note: string, active: boolean }
+
+    const dispatch = createEventDispatcher()
+
+    function passClue() {
+        dispatch('message', {
+            clueType: clue.id
+        })
+    }
+</script>
+
+<li class="hover:text-black disabled:cursor-auto w-full py-1 px-2.5 cursor-pointer bg-gray-700 hover:bg-gray-200 disabled:bg-gray-400">
+    <button 
+        id={clue.id}
+        on:click={passClue}
+        disabled={clue.active}
+    >
+        {clue.note}
+    </button>
+</li>
