038617375e9ccdc85fe23eea5c35f2d7151d0d04
cdrani <charlesdrani@gmail.com>
2024-04-01 18:00:09 -0600

    feat: create Keys & Key components

---
+++ src/lib/components/Key.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let key: string;
+    export let width: number = 8;
+</script>
+
+<button name={key} class="h-10 w-{width} rounded-sm bg-gray-700 hover:bg-gray-500">
+    <span class="text-sm font-semibold uppercase">{key}</span>
+</button>
---
+++ src/lib/components/Keys.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import Key from "./Key.svelte";
+
+    const firstRow = 'qwertyuiop'.split('')
+    const secondRow = 'asdfghjkl'.split('')
+    const lastRow = 'zxcvbnm'.split('')
+</script>
+
+<div class="flex-col justify-center left-1/2 -translate-x-1/2 items-center w-full px-4 absolute bottom-6">
+    <div class="flex justify-center gap-x-2 items-center">
+        {#each firstRow as key (key)}
+            <Key key={key} />
+        {/each}
+    </div>
+
+    <div class="flex justify-evenly gap-x-2 mx-4 items-center my-2">
+        {#each secondRow as key (key)}
+            <Key key={key} />
+        {/each}
+    </div>
+
+    <div class="flex justify-evenly gap-x-2 items-center w-full">
+        <Key key="go" width={12} />
+
+        {#each lastRow as key (key)}
+            <Key key={key} />
+        {/each}
+
+        <Key key="del" width={12} />
+    </div>
+</div>
