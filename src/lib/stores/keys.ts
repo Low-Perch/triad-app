dfd791f9063728fe367d79ed1c3022cf63a0d254
cdrani <charlesdrani@gmail.com>
2024-04-03 16:56:40 -0600

    feat: create keys store and action to disable keys

---
+++ src/lib/stores/keys.ts
@@ -0,0 +1,46 @@
+import { writable } from 'svelte/store'
+import type { Writable } from 'svelte/store'
+import { getContext, setContext } from 'svelte'
+
+const STORE = 'keys'
+
+export type Keys = { keysDisabled: boolean, disabledKeys: string[] }
+type Context = Writable<Keys>
+
+export const INIT_KEYS: Keys = { disabledKeys: [], keysDisabled: false }
+
+const keys = writable(INIT_KEYS)
+
+export function initKeys() {
+    setContext(STORE, keys)
+}
+
+export function getKeys() {
+    return getContext<Context>(STORE)
+}
+
+function shuffleArray(array: string[]): string[] {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        const temp = array[i]
+
+        array[i] = array[j]
+        array[j] = temp
+    }
+
+    return array
+}
+
+export function disableKeys(puzzleKey: string) {
+    keys.update(prevState => {
+        const ALPHA = 'qwertyuiopasdfghjklzxcvbnm'
+        const keysToDisable = ALPHA.split('').filter((char: string) => {
+            return !puzzleKey.toLowerCase().includes(char)
+        })
+
+        const shuffledKeys = shuffleArray(keysToDisable)
+        const disabledKeys = shuffledKeys.slice(0, 13)
+
+        return { ...prevState, disabledKeys, keysDisabled: true }
+    })
+}
