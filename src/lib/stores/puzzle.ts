73e8b357bb802b0e9b8391455250ee644ec1b7bb
cdrani <charlesdrani@gmail.com>
2024-04-03 08:45:44 -0600

    feat: create puzzle store

---
+++ src/lib/stores/puzzle.ts
@@ -0,0 +1,36 @@
+import { writable } from 'svelte/store'
+import type { Writable } from 'svelte/store'
+import { getContext, setContext } from 'svelte'
+
+const STORE = 'puzzle'
+
+export enum PuzzleState {
+    START = 'start',
+    CLUE = 'clue',
+    SOLUTION = 'solution'
+}
+
+export type Puzzle = { state: PuzzleState, start: string, clue: string, solution: string }
+type Context = Writable<Puzzle>
+
+export const INIT_PUZZLE: Puzzle = {
+    state: PuzzleState.START,
+    start: 'WARE / REAF / CON',
+    clue: 'WARE____ / REAF / CON', 
+    solution: 'FIRMWARE / REAFFIRM / CONFIRM'
+}
+
+const puzzle = writable(INIT_PUZZLE)
+
+export function initPuzzle() {
+    setContext(STORE, puzzle)
+}
+
+export function getPuzzle() {
+    return getContext<Context>(STORE)
+}
+
+export function updatePuzzleState(state: PuzzleState) {
+    console.log({ pz: state })
+    puzzle.update(prevState => ({...prevState, state }))
+}
