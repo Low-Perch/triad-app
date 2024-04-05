2f15b719ef0c32e728e1cb9ca25fa05565e28da0
cdrani <charlesdrani@gmail.com>
2024-04-05 11:29:51 -0600

    feat: add lock icon on locked key

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+    import { keyLocked } from '../stores/clues'
+    import { getPuzzle } from '../stores/puzzle'
     import { getInput, InputState } from '../stores/input'
 
     const input = getInput()
+    const puzzle = getPuzzle()
 
     $: correct = $input.state == InputState.CORRECT
     $: incorrect = $input.state == InputState.INCORRECT
+    $: showLock = $keyLocked && !$puzzle.solved
 </script>
 
 <div class="flex justify-center items-center my-8 h-40">
@@ -13,9 +17,15 @@
             class:solved={correct}
             class:shake={incorrect}
             class:bg-green-500={correct}
-            class="box flex m-4 aspect-square border-white border-2 w-12 h-12 justify-center place-items-center"
+            class="relative box flex m-4 aspect-square border-white border-2 w-12 h-12 justify-center place-items-center"
         >
             <p class="text-white uppercase text-3xl">{key}</p>
+
+            {#if showLock && i == $input.keys.length - 1}
+                <svg viewBox="0 0 24 24" class="absolute bg-transparent -bottom-4 w-6 h-6 fill-white">
+                    <path d="m18 8h-1v-2c0-2.76-2.24-5-5-5s-5 2.24-5 5v2h-1c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-10c0-1.1-.9-2-2-2zm-6 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zm3.1-9h-6.2v-2c0-1.71 1.39-3.1 3.1-3.1s3.1 1.39 3.1 3.1z"/>
+                </svg>
+            {/if}
         </div>
     {/each}
 </div>
